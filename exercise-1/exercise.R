731aa47a60d436814af5e2efce1a9382f6a61ef5
Niva Ashkenazi <nivaash@uw.edu>
2017-11-14 13:53:37 -0800

    graph the function using buildScatter function in other R file

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,6 +4,8 @@
 
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("scripts/BuildScatter.R")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatter(iris, iris$Sepal.Length, iris$Sepal.Width, iris$Species, 
+             title = "iris data", xlab = "sepal length", ylab = "sepal width")
