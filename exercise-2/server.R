10c491384571525def87b42e19449cbcdb24fa21
Andrey Butenko <ghostmancer@gmail.com>
2017-11-14 20:04:18 -0800

    Complete exercise 2

--- exercise-2/server.R
+++ exercise-2/server.R
@@ -1,6 +1,7 @@
 ### Exercise 2 ###
 
 library(shiny)
+library(ggplot2)
 
 # We'll look into these more next week: http://shiny.rstudio.com/gallery/widget-gallery.html 
 
@@ -8,7 +9,25 @@ library(shiny)
 
 # It should takes as an input the number of observations, and a color
 # It should return a rendered plot
+
 shinyServer(function(input, output) {
-  # Save a 'scatter' property which is a renderPlot object (that renders a scatterplot)
+  GetDataFrame <- reactive({
+    number.observations <- input$numObs
+    
+    df <- data.frame(rnorm(number.observations), rnorm(number.observations))
+    colnames(df) <- c('x', 'y')
+    
+    return(df)
+  })
   
+  output$scatter <- renderPlot({
+    df <- GetDataFrame()
+    color <- input$color
+    
+    scatter <- ggplot(data = df) +
+      geom_point(mapping = aes(x = df$x, y = df$y), color = color, size = 5)  +
+      labs(x = 'x', y = 'y')
+    
+    return(scatter)
+  })
 })
--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -4,12 +4,14 @@
 shinyUI(fluidPage(
   
   # Add a numbericInput to choose the number of observations
-  
+  sliderInput('numObs', 'Number of observations',
+               20, 100000, 50, step = 10),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput('color', 'Color',
+              c('Red', 'Green', 'Blue', 'Purple')),
   
   # Plot the output with the name "scatter"
-  
+  plotOutput('scatter')
   
 ))
